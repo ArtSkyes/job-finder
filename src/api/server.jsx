30d27590fb395f9ab546eac9c31553facc97d563
Art <honeydewart@gmail.com>
2024-05-14 14:50:43 +0800

    changing the proxy

--- src/api/server.jsx
+++ src/api/server.jsx
@@ -5,20 +5,10 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-server.use((req, res, next) => {
-    console.log('Request:', req.method, req.url)
-    next()
-})
-
 server.use(middlewares)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1'
 }))
 server.use(router)
-
-server.use((err, req, res, next) => {
-    console.error('Error:', err.message)
-    next(err)
-})
 
 module.exports = server
