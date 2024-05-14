26a370324e27703f535e888c968d27eec71a50a4
Art <honeydewart@gmail.com>
2024-05-14 08:39:06 +0800

    created production folder

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-})
+});
