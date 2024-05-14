fb5eda80913df75450602cef0a16c2c3b9afd1d9
Art <honeydewart@gmail.com>
2024-05-14 15:25:28 +0800

    fixed file path

--- src/api/server.js
+++ src/api/server.js
@@ -1,22 +1,17 @@
 const jsonServer = require('json-server')
+const path = require('path')
+const fs = require('fs')
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
+const filePath = path.join(__dirname, '../db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db)
 
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
@@ -26,5 +21,4 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
