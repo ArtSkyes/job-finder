08969f3154591ed74b97b137bbcc601c51f80da0
Art <honeydewart@gmail.com>
2024-05-14 15:40:41 +0800

    reverted changes

--- src/api/server.js
+++ src/api/server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 const server = jsonServer.create()
 
-const filePath = path.join(__dirname, 'db.json')
+const filePath = path.join(__dirname, '../db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db)
